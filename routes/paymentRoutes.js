c098458806aada1696d61ceaff6bc97b7b19a7a2
Ashish Jha <ashisjha1123@gmail.com>
2026-09-04 14:13:09 +0530

    Add Razorpay payment and webhook verification

---
+++ routes/paymentRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { verifyPayment }  = require('../controller/paymentController');
+const { protect } = require('../middlewares/protect');
+const  admin  = require('../middlewares/admin');
+
+const router = express.Router();
+
+router.route('/verify').post(protect, verifyPayment);
+
+
+module.exports = router;
